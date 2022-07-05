d07c8699797adf9183fd9f88f4e47a53a0a3ae18
Jesper Kristensen <cableman@linuxdev.dk>
2022-07-05 20:35:48 +0200

    DDBTEAM-948: Fixed return types

--- src/ItkDevMetricsBundle.php
+++ src/ItkDevMetricsBundle.php
@@ -3,6 +3,7 @@
 namespace ItkDev\MetricsBundle;
 
 use ItkDev\MetricsBundle\DependencyInjection\ItkDevMetricsExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ItkDevMetricsBundle extends Bundle
@@ -10,7 +11,7 @@ class ItkDevMetricsBundle extends Bundle
     /**
      * Overridden to allow for the custom extension alias.
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new ItkDevMetricsExtension();
